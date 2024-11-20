74125a64bc801c36be1b0cac20f12632e6d2ff1a
MF <mfregwork21@gmail.com>
2024-11-20 10:42:43 -0800

    Successfully rendered all users that are online

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 
 function Chat() {
     const [ws, setWs] = useState(null)
+    const [onlineUsers, setOnlineUsers] = useState({})
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:3000')
         setWs(ws)
@@ -23,7 +24,8 @@ function Chat() {
         usersArray.forEach(({userId, username}) => {
             users[userId] = username
         })
-        console.log(users)
+        // console.log(users)
+        setOnlineUsers(users)
     }
 
     function handleMessage(e){
@@ -41,7 +43,13 @@ function Chat() {
   return (
     <div className="flex h-screen">
         <div className="bg-white w-1/3">
-            left
+            {Object.keys(onlineUsers).map((userId, index) => (
+                <div key={index}>
+                    {/* map through each users username */}
+                    {/* {userId} */}
+                    {onlineUsers[userId]}
+                </div>
+            ))}
         </div>
         <div className="flex flex-col bg-blue-400 w-2/3 p-3">
             <div className="flex-grow">
@@ -56,7 +64,6 @@ function Chat() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}       stroke="currentColor" className="size-5 stroke-1 hover:stroke-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                     </svg>
-
                 </button>
             </div>
         </div>
